f2f8b2eb770ca56c373bc64c6beeaf52bd083d61
zahorodniairyna <143090318+zahorodniairyna@users.noreply.github.com>
2023-08-24 15:00:00 +0300

    Create send.php

---
+++ send.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Remove Your Data from the Game</title>
+  </head>
+  <body>
+    <p>To remove your data from the game, please email our support team at <a href="mailto:zahorodniairyna1995@gmail.com">zahorodniairyna1995@gmail.com</a>. We will definitely review your request within 24 hours.</p>
+  </body>
+</html>
+
